6537d243cc11ebd6cc16a4693c7381a07a0b44cf
faisalsahak <sahak.faisal@gmail.com>
2022-03-15 18:47:43 -0700

    added the get,post and delete routes using FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,79 @@
-from fastapi import FastAPI
+
+
+
+from fastapi import Body, FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {'message':'helloworld'}
+    return {'message':'hessfdasdfafdloworld'}
+
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title or post 1", "content":"content of post 1", "id":1},
+            {"title":"my favoutiret food", "content":"i love pizza", "id":2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+@app.get('/')
+def root():
+    return {"message": 'Hello world'}
+
+@app.get('/posts')
+def get_posts():
+    return {'data': my_posts}
+
+
+
+@app.post('/posts', status_code=status.HTTP_201_CREATED )
+def create_posts(post: Post):
+    # print(post.title)
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get('/posts/{id}')
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+         detail=f"post with id: {id} not found")
+    return {"post_detail": post} 
+
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} does not exist' )
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+
+# @app.get('/posts/latest')
+# def get_latest_post():
+#     post = my_posts[len(my_posts)-1]
+#     return {"detail": post}
